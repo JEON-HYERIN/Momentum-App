3e2be1a600ac3672ec479e3377ed889fc54ae596
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-11-16 23:37:36 +0900

    html수정

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,11 @@
-const h2El = document.querySelector('.clock h2');
+const emEl = document.querySelector('.clock em');
 
 function getClock () {
   const date = new Date();
   const hour = String(date.getHours()).padStart(2,'0');
   const minute = String(date.getMinutes()).padStart(2,'0');
   // const seconds = String(date.getSeconds()).padStart(2,'0');
-  h2El.textContent = `${hour}:${minute}`;
+  emEl.textContent = `${hour}:${minute}`;
 }
 getClock();
 setInterval(getClock, 1000);
